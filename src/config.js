c71f9a6d89db5c1d7d2937e1439ee03148f17f49
marcosgomezsanchez <marcosgomezsanchez@gmail.com>
2021-02-24 10:35:40 +0100

    clone

---
+++ src/config.js
@@ -0,0 +1,13 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+    consumer_key: 'put your consumer key',  
+    consumer_secret: 'put your consumer secret',
+    access_token: 'put your access token',  
+    access_token_secret: 'put your access token secret'
+  }
